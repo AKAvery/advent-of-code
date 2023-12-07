7d0d5d19f04795ccc0024f144bcc3d282bea7448
AKAvery <134798000+AKAvery@users.noreply.github.com>
2023-12-07 05:23:46 +0000

    day 1 struggle changes

--- solutions/day_1.py
+++ solutions/day_1.py
@@ -2,17 +2,32 @@ input = 'first.txt'
 final = 0
 with open(input, 'r') as f:
     original = f.readlines()
-new = 0
+
+numbers = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
 for lines in original:
     first = None
     last = None
-    for char in lines: 
+    for index, char in enumerate(lines): 
         if char.isdigit() == True:
             last = char
-    for char in lines:
+    
+        for i in range(6):
+            if lines[index:index+1] in numbers:
+                last = numbers[lines[index:index+i]]
+        
+    for index, char in enumerate(lines):
         if char.isdigit() == True:
-            first = char
-            break
+            if first == None:
+                first = char
+                break
+        
+        for i in range(6):
+            if first == None:
+                if lines[index:index+i] in numbers:
+                    first = numbers[lines[index:index+i]]
+                    break
+
+            
     if first is not None:
         single = first + last
         final += int(single)
